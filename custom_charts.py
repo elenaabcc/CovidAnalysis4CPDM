32e0227e4d599dcea3e4a2508a44010ff41fec85
Elena Martarello <Martarelloe@gmail.com>
2022-01-15 16:30:32 +0100

    Fix generate figure function

--- custom_charts.py
+++ custom_charts.py
@@ -7,3 +7,10 @@ def generateTimeSeriesChartOverYears(dataframesMatrix, xaxis, yaxis, palette):
         for j, df in enumerate(dfs):
             fig.add_trace(go.Scatter(x = df[xaxis], y=df[yaxis], line = dict(width=2, color = palette[j]), stackgroup=str(j)), row=i+1, col=1)
     return fig
+
+def generateTimeSeriesChartWithSubplots(dataFrames, titles, xaxisLabel, yaxisLabel):
+    fig = make_subplots(rows=len(dataFrames), cols=1,vertical_spacing=0.1)
+    # TODO: Add title in each subplot
+    for i, df in enumerate(dataFrames):
+        fig.add_trace(go.Scatter(x = df[xaxisLabel], y=df[yaxisLabel], line = dict(width=2)), row=i+1, col=1)
+    return fig
