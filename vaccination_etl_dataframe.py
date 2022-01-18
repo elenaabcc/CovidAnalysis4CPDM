b547b670d6a55f8eba73b8e4e471ec71873d4ade
Elena Martarello <Martarelloe@gmail.com>
2022-01-18 10:52:12 +0100

    clear folders names to make a standards

---
+++ vaccination_etl_dataframe.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import os
+import glob
+
+# https://ourworldindata.org/covid-vaccinations
+data_covid19_owid = pd.read_csv("./covid19_dataframes/owid-covid-data.csv")
+data_covid19_owid = data_covid19_owid.query("iso_code == 'ITA'")
+
+data_covid19_owid = data_covid19_owid[['iso_code','date', 'location', 'continent', 'new_cases', 'new_deaths', 'total_cases', 
+                                        'total_deaths', 'reproduction_rate', 'icu_patients', 'aged_65_older', 'aged_70_older',
+                                        'median_age', 'extreme_poverty', 'gdp_per_capita', 'cardiovasc_death_rate', 'diabetes_prevalence',
+                                        'new_tests', 'total_tests','positive_rate', 'excess_mortality'
+                                    ]]
+
+
+# https://www.ecdc.europa.eu/en/publications-data/data-covid-19-vaccination-eu-eea
+data_covid19_ecdc= pd.read_csv("./covid19_dataframes/data_covid19_vaccination.csv")
+data_covid19_ecdc = data_covid19_ecdc.query("ReportingCountry == 'IT'")
