fc5986053fb97471df5d45980e144b9e63097f46
Elena Martarello <Martarelloe@gmail.com>
2022-02-15 22:42:40 +0100

    add store part

--- vaccination_ue_analysis/etl_vaccination_dataframe.py
+++ vaccination_ue_analysis/etl_vaccination_dataframe.py
@@ -4,7 +4,7 @@ import os
 import glob
 
 # https://ourworldindata.org/covid-vaccinations
-data_covid19_owid = pd.read_csv("./covid19_dataframes/owid-covid-data.csv")
+data_covid19_owid = pd.read_csv("owid-covid-data.csv")
 data_covid19_owid = data_covid19_owid.query("iso_code == 'ITA'")
 
 data_covid19_owid = data_covid19_owid[['iso_code','date', 'location', 'continent', 'new_cases', 'new_deaths', 'total_cases', 
@@ -15,7 +15,7 @@ data_covid19_owid = data_covid19_owid[['iso_code','date', 'location', 'continent
 
 
 # https://www.ecdc.europa.eu/en/publications-data/data-covid-19-vaccination-eu-eea
-data_covid19_ecdc= pd.read_csv("./covid19_dataframes/data_covid19_vaccination.csv")
+data_covid19_ecdc= pd.read_csv("data_covid19_vaccination.csv")
 data_covid19_ecdc.drop(['NumberDosesReceived','NumberDosesExported', 'FirstDoseRefused'], axis=1, inplace=True)
 
 splitedYearWeekISO = data_covid19_ecdc["YearWeekISO"].str.split("-", n = 1, expand = True)
